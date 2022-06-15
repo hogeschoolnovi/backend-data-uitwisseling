1a588d23a0682118d825cba0f9bf43d801233850
Whitedragon1987 <vanoostenjohan@hotmail.com>
2022-06-15 12:31:02 +0200

    id_sequence

--- src/main/java/com/example/communicatie/model/Student.java
+++ src/main/java/com/example/communicatie/model/Student.java
@@ -1,12 +1,24 @@
 package com.example.communicatie.model;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
 import javax.persistence.*;
 
 @Entity
 public class Student {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "user_sequence"),
+                    @Parameter(name = "initial_value", value = "1003"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
     private Long studentNumber;
     
     private String emailAddress;
